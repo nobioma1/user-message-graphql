888a0d588a3cf4cb7ebffeaadefd312264be0ebc
Noble Obioma <nobioma1@gmail.com>
2019-08-24 19:38:06 +0100

    ft: Create Query Schema for users and user, Create Resolvers to get all user and a particular user

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const { ApolloServer, gql } = require('apollo-server-express');
 
+const { usersData } = require('./data');
+
 const app = express();
 
 app.use(cors());
@@ -9,12 +11,24 @@ app.use(cors());
 const schema = gql`
   type Query {
     info: String!
+    users: [User!]
+    user(id: ID!): User
+  }
+
+  type User {
+    id: ID!
+    username: String!
   }
 `;
 
 const resolvers = {
   Query: {
     info: () => 'Welcome to Learn-GraphQL',
+    users: () => usersData,
+    user: (root, args) => {
+      const { id } = args;
+      return usersData.find(user => user.id === id);
+    },
   },
 };
 
