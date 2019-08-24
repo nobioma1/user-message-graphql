004b53e060c27e98337b0f11c6e283600050e902
Noble Obioma <nobioma1@gmail.com>
2019-08-24 19:35:59 +0100

    ch: Create sample data file for users

---
+++ src/data.js
@@ -0,0 +1,16 @@
+module.exports = {
+  usersData: [
+    {
+      id: '1',
+      username: 'john',
+    },
+    {
+      id: '2',
+      username: 'jane',
+    },
+    {
+      id: '3',
+      username: 'fred',
+    },
+  ],
+};
