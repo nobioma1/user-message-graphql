ca66686450675d5b189ad7e09bca7e72082281ce
Noble Obioma <nobioma1@gmail.com>
2019-08-25 11:10:04 +0100

    Add email field to User type, create Token type and Mutations to sign up and Login

--- src/schema/user.js
+++ src/schema/user.js
@@ -4,11 +4,21 @@ module.exports = gql`
   type User {
     id: ID!
     username: String!
+    email: String!
     messages: [Message!]
+  }
+
+  type Token {
+    token: String!
   }
 
   extend type Query {
     users: [User!]
     user(id: ID!): User
+  }
+
+  extend type Mutation {
+    signup(username: String!, email: String!, password: String!): Token!
+    login(cred: String!, password: String!): Token!
   }
 `;
