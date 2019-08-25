d349e1bff755476f43e24ddb25dcb690f540d669
Noble Obioma <nobioma1@gmail.com>
2019-08-25 09:10:23 +0100

    Turn off sequelize logging, and remove id from messages seeds

--- src/seeders/20190824230335-messages.js
+++ src/seeders/20190824230335-messages.js
@@ -6,21 +6,18 @@ module.exports = {
       'Messages',
       [
         {
-          id: 1,
           text: 'Alo, How are you?',
           userId: '78d9446b-e132-4dc0-b7f7-abb420fd6ddf',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          id: 2,
           text: `Hi, I'm learning GraphQL`,
           userId: '407feede-3315-423f-9735-af880bdb3a9c',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          id: 3,
           text: "Hi, I'm another message by user id 1",
           userId: '78d9446b-e132-4dc0-b7f7-abb420fd6ddf',
           createdAt: new Date(),
