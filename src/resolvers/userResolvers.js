6eaabe50af14a8577a40b78e94908637f97464b0
Noble Obioma <nobioma1@gmail.com>
2019-08-25 11:15:18 +0100

    Create resolvers for login and signup

--- src/resolvers/userResolvers.js
+++ src/resolvers/userResolvers.js
@@ -1,3 +1,6 @@
+const { UserInputError, AuthenticationError } = require('apollo-server');
+const { generateToken } = require('../utils/authToken');
+
 module.exports = {
   Query: {
     users: async (parent, args, { models }) => {
@@ -7,6 +10,39 @@ module.exports = {
     user: async (parent, args, { models }) => {
       const { User } = models;
       return await User.findByPk(args.id);
+    },
+  },
+  Mutation: {
+    signup: async (parent, args, { models }) => {
+      const { User } = models;
+      const { username, email, password } = args;
+      try {
+        const user = await User.create({
+          username,
+          email,
+          password,
+        });
+        const token = generateToken(user);
+
+        return { token };
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    login: async (parent, args, { models }) => {
+      const { User } = models;
+      // (cred) - username or email
+      const { cred, password } = args;
+      const user = await User.findByCred(cred);
+      if (!user) {
+        throw new UserInputError('User with credential does not exist');
+      }
+      const isValid = await user.validatePassword(password);
+      if (!isValid) {
+        throw new AuthenticationError('Invalid Password');
+      }
+      const token = generateToken(user);
+      return { token };
     },
   },
 };
