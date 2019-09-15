f186454f386940443f793da4a62363c0cc2e3059
Noble Obioma <nobioma1@gmail.com>
2019-09-15 13:33:18 +0100

    Create resolver to check if authUser is an admin

--- src/resolvers/authorization.js
+++ src/resolvers/authorization.js
@@ -1,17 +1,14 @@
 const { ForbiddenError } = require('apollo-server');
 const { skip } = require('graphql-resolvers');
 
-const isAuthenticated = (parent, args, context) => {
-  const { authUser } = context;
+const isAuthenticated = (parent, args, { authUser }) => {
   if (!authUser) {
     return new ForbiddenError('User not Authenticated');
   }
   return skip;
 };
 
-const isAuthor = async (parent, args, context) => {
-  const { id } = args;
-  const { models, authUser } = context;
+const isAuthor = async (parent, { id }, { models, authUser }) => {
   const message = await models.Message.findByPk(id, { raw: true });
 
   if (message.userId !== authUser.id) {
@@ -20,7 +17,17 @@ const isAuthor = async (parent, args, context) => {
   return skip;
 };
 
+const isAdmin = (parent, args, { authUser: { role } }) => {
+  if (role === 'admin') {
+    return skip;
+  }
+  return new ForbiddenError(
+    'User does not have authorization for this operation'
+  );
+};
+
 module.exports = {
   isAuthenticated,
   isAuthor,
+  isAdmin,
 };
